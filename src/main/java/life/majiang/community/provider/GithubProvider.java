1a5a4c30e9828a248632b2526be0b4fa60ec80c8
AllenChou124 <zhoutmicallen@gmail.com>
2021-01-22 10:22:37 +0800

    extract github config from code to properties

---
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,57 @@
+package life.majiang.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+/**
+ * @author 12418
+ */
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                String string = response.body().string();
+                System.out.println(string);
+                //得到了String类型的accessToken 样子为access_token=xxxx&scope=xx&token_type=xx分解这个
+                String[] split = string.split("&");
+                String tokenstr = split[0];
+                String token = tokenstr.split("=")[1];
+                System.out.println(token);
+                return token;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user")
+                .header("Authorization","token "+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string,GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
